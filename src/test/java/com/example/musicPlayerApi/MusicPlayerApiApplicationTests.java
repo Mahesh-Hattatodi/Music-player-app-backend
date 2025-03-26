316f06aa52904b2a64a29f035a7808966e6d5ae5
Mahesh-Hattatodi <maheshh1323@gmail.com>
2025-03-26 22:28:56 +0530

    Initial commit

---
+++ src/test/java/com/example/musicPlayerApi/MusicPlayerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicPlayerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlayerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
